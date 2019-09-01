89269dcbd2457bb3eb5ffe68d60d12444b17ebc8
George499 <gi.32@mail.ru>
2019-09-01 18:45:10 +0300

    added menu js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
-var obj = {
-    name: "я",
-    lastname: "Георгий",
-    mynickname: "George Isaev"
-};
-
-console.log(obj.name, obj.lastname, obj.mynickname);
+let menu__item = document.querySelectorAll("#menu__item");
+for (let i = 0; i < menu__item.length; i++) {
+menu__item[i].addEventListener("click", function() {
+    menu__item[i].classList.toggle('menu__item--active')})
+}
