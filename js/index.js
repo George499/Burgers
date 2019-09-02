59a7d6b7a7adb272af93e31507163567f4a37136
George499 <gi.32@mail.ru>
2019-09-02 03:28:53 +0300

    1

--- js/index.js
+++ js/index.js
@@ -10,3 +10,9 @@ for (let i = 0; i < team__item.length; i++) {
         team__item[i].classList.toggle("team__item--active")
     })
 }
+
+let header__menu__link = document.querySelectorAll("#header__menu__link");
+let hamburger__fullscreen = document.querySelectorAll("#hamburger__fullscreen");
+header__menu__link.addEventListener ("click", function(){
+    
+})
